a331ccf1a56ee5ccde8a29efe91c368b4fc31693
Isaque <isaquecoelho.dj@gmail.com>
2025-11-23 15:08:49 -0300

    Chore: Adiciona docstrings no cartao.py

--- simple_factory/cartao.py
+++ simple_factory/cartao.py
@@ -3,4 +3,5 @@ from pagamento import Pagamento
 # Produto concreto
 class Cartao(Pagamento):
     def pagar(self) -> str:
+        """Retorna a confirmação da geração do pagamento por cartão."""
         return "Pagamento realizado com cartão de crédito/débito!"
