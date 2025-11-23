0dc97a4e432a6eb12609593f5eb32feeb821a3e9
Isaque <isaquecoelho.dj@gmail.com>
2025-11-23 00:37:03 -0300

    Refatora: Reestrutura main.py e atualiza chamadas da Factory
    - Move o código de demonstração para a função 'demonstrarPaganentos()'.
    - Atualiza as chamadas à PagamentoFactory para usar 'criarPagamentoOnline' conforme refatoração.
    - Adiciona importação da NotificacaoFactory para futuras demonstrações.

--- simple_factory/main.py
+++ simple_factory/main.py
@@ -1,14 +1,22 @@
 from pagamento_factory import PagamentoFactory
+from notificacao_factory import NotificacaoFactory
 
 # Uso
+def demonstrarPaganentos():
+    pagamento_cartao = PagamentoFactory.criarPagamentoOnline("cartão")
+    print(pagamento_cartao.pagar()) 
+
+    # pagamento1 = PagamentoFactory.criarPagamento("pix")
+    # print(pagamento1.pagar()) 
+
+    # pagamento1 = PagamentoFactory.criarPagamento("boleto")
+    # print(pagamento1.pagar()) 
+
+    # notificao1 = NotificacaoFactory.criarNotificacao("email")
+    # print(notificao1.enviar())
+
 if __name__ == "__main__":
-    pagamento1 = PagamentoFactory.criarPagamento("cartão")
-    print(pagamento1.pagar()) 
+    demonstrarPaganentos()
 
-    pagamento1 = PagamentoFactory.criarPagamento("pix")
-    print(pagamento1.pagar()) 
-
-    pagamento1 = PagamentoFactory.criarPagamento("boleto")
-    print(pagamento1.pagar()) 
 
    
