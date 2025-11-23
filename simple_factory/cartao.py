b710f8aad6a27c85bb0da3d0b95ed0efe829b76a
Isaque <isaquecoelho.dj@gmail.com>
2025-11-22 23:24:21 -0300

    Melhora: Mensagem de retorno da classe Cartao

--- simple_factory/cartao.py
+++ simple_factory/cartao.py
@@ -3,4 +3,4 @@ from pagamento import Pagamento
 # Produto concreto
 class Cartao(Pagamento):
     def pagar(self) -> str:
-        return "Pago com cartão!"
+        return "Pagamento realizado com cartão de crédito/débito!"
